2627f5c95b2c3d51c592a8831e5ce66ba2442125
Ryan Cumming <etaoins@gmail.com>
2020-07-21 16:17:58 +1000

    feat(types): Export types (#8)
    This is helpful for annotating helper functions etc. in consumer code.
    We're already using these types in our public interface but they're not
    named.
    
    Apparently you can't use `export type *` so this just names our existing
    types. This might be safer if we add some internal types but it'll make
    it easy to forget new types in the future.

--- src/fakeHr.ts
+++ src/fakeHr.ts
@@ -1,2 +1,8 @@
 export * as competencies from './competencies';
 export * as education from './education';
+export type {
+  CountryCode,
+  EducationInstitution,
+  EducationLevel,
+  EducationQualification,
+} from './types';
