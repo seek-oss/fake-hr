1b66b187fbd680f47c6a480baaf712aa479123f4
Ryan Ling <ryan@outlook.com.au>
2022-03-24 16:50:40 +1100

    Uplift workflows
    - Fix CodeQL branch warning
    - Lock down permissions
    - Pin action versions
    - Remove unnecessary LFS support

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,14 @@ const { Jest } = require('skuba');
 
 module.exports = Jest.mergePreset({
   coveragePathIgnorePatterns: ['src/testing'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
   setupFiles: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ['/test\\.ts'],
 });
